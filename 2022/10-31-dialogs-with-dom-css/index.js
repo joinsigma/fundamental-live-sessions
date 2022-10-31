f921526b981ba0f49607ba33848ea4432ab6073e
danishrashidin <danishrashidin@gmail.com>
2022-10-31 21:39:32 +0700

    🔨 Added 31st Oct - Dialogs with DOM and CSS

---
+++ 2022/10-31-dialogs-with-dom-css/index.js
@@ -0,0 +1,15 @@
+function showDialog() {
+    // Get the dialog
+    let dialogElement = document.getElementById("dialog");
+
+    // Change that element's display property to block
+    dialogElement.style.display = "block";
+}
+
+function hideDialog() {
+    // Get the dialog
+    let dialogElement = document.getElementById("dialog");
+
+    // Change that element's display property to none
+    dialogElement.style.display = "none";
+}
