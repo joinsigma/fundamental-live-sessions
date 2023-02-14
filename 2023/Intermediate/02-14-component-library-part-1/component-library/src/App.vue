518247a052a6153614d6fedbba73c5ed6718541f
danishrashidin <danishrashidin@gmail.com>
2023-02-14 22:11:19 +0800

    update

---
+++ 2023/Intermediate/02-14-component-library-part-1/component-library/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+import Button from "./components/Botton.vue"
+
+function printB() {
+  console.log("B")
+}
+
+</script>
+
+<template>
+  <div class="h-96 w-full bg-gray-200 flex flex-col gap-y-4 justify-center items-center">
+    <Button title="Button Top" @click="printB" />
+    <Button title="Button Bottom" />
+  </div>
+</template>
---
+++ 2023/Intermediate/02-14-component-library-part-1/component-library/src/components/Botton.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+const props = defineProps(["title"])
+
+</script>
+
+<template>
+    <button type="button"
+        class="px-3 py-2 shadow-lg self-center bg-blue-400 rounded-lg text-white font-medium hover:bg-green-500">{{
+    props.title
+        }}</button>
+</template>
