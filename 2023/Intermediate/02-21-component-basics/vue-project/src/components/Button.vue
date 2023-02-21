d6785659c83e59822b4fb765bbf66b74238e7cf4
danishrashidin <danishrashidin@gmail.com>
2023-02-21 21:53:40 +0800

    update: session content

---
+++ 2023/Intermediate/02-21-component-basics/vue-project/src/components/Button.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+// Props = Properties
+const props = defineProps(["title", "prefix"])
+const content = props.prefix + props.title
+
+</script>
+
+<template>
+    <!--Template syntax-->
+    <button>{{ content }}</button>
+</template>
+
+<style>
+button {
+    padding: 12px;
+    background-color: darkslategray;
+    color: white;
+}
+</style>
